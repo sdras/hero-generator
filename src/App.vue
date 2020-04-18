1a86311b955f40cd56d47d59b99181e2b76dd7fa
Sarah Drasner <sarah.drasner@gmail.com>
2020-04-18 09:03:52 -0600

    add in hero image

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,17 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <app-masthead />
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import AppMasthead from "./components/AppMasthead.vue";
 
 export default {
-  name: 'App',
   components: {
-    HelloWorld
+    AppMasthead
   }
-}
+};
 </script>
 
 <style lang="scss">
---
+++ src/components/AppMasthead.vue
@@ -0,0 +1,40 @@
+<template>
+  <section class="masthead"></section>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style lang="scss" scoped>
+.masthead {
+  width: 100%;
+  height: 60vh;
+  overflow: hidden;
+  background: -moz-radial-gradient(
+      center,
+      ellipse cover,
+      rgba(0, 0, 0, 0) 0%,
+      rgba(0, 0, 0, 0) 37%,
+      rgba(0, 0, 0, 0.65) 100%
+    ),
+    url("../assets/alberto-restifo.png") no-repeat center center scroll; /* FF3.6-15 */
+  background: -webkit-radial-gradient(
+      center,
+      ellipse cover,
+      rgba(0, 0, 0, 0) 0%,
+      rgba(0, 0, 0, 0) 37%,
+      rgba(0, 0, 0, 0.65) 100%
+    ),
+    url("../assets/alberto-restifo.png") no-repeat center center scroll; /* Chrome10-25,Safari5.1-6 */
+  background: radial-gradient(
+      ellipse at center,
+      rgba(0, 0, 0, 0) 0%,
+      rgba(0, 0, 0, 0) 37%,
+      rgba(0, 0, 0, 0.65) 100%
+    ),
+    url("../assets/alberto-restifo.png") no-repeat center center scroll; /* W3C, IE10+, FF16+, Chrome26+, Opera12+, Safari7+ */
+  background-size: cover;
+  color: #333;
+}
+</style>
