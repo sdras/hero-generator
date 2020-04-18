7907537da28f669ab7f83fa393cbc905b379362c
Sarah Drasner <sarah.drasner@gmail.com>
2020-04-18 08:39:21 -0600

    start bringing in Ant design

--- src/main.js
+++ src/main.js
@@ -1,10 +1,13 @@
-import Vue from 'vue'
-import App from './App.vue'
-import store from './store'
+import Vue from "vue"
+import Antd from "ant-design-vue"
+import App from "./App.vue"
+import "ant-design-vue/dist/antd.css"
+import store from "./store"
 
 Vue.config.productionTip = false
+Vue.use(Antd)
 
 new Vue({
   store,
-  render: h => h(App)
-}).$mount('#app')
+  render: (h) => h(App),
+}).$mount("#app")
